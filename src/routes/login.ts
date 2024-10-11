fa05206c65e8cd5424d6857d6c17dd8db426a818
João Vítor Ferraz <git@p1pa.dev>
2024-10-10 23:12:18 -0300

    refactor: login route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -5,15 +5,13 @@ import * as userController from '../controllers/userController';
 const router = express.Router();
 
 router.get('/', userController.loginUserGET);
-
-router.post(
-  '/',
+router.post('/', [
   userController.loginUserPOST,
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureMessage: true,
-  })
-);
+  }),
+]);
 
 export default router;
